35bc08f6b19cda6e34a51c3e653a9cfa34d54a4a
OlehMartyniak <97449514+OlehMartyniak@users.noreply.github.com>
2022-08-20 14:40:48 +0300

    Moved props from parent to child

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from "react";
 
 import './App.css';
 import Content from './Content';
@@ -6,11 +7,44 @@ import Header from './Header';
 import Footer from './Footer';
 
 const App = () => {
+
+  const groceryList = [
+    {
+        id: 1,
+        checked: false,
+        item: "Tomatoes"
+    },
+    {
+        id: 2,
+        checked: false,
+        item: "Orange Juice"
+    },
+    {
+        id: 3,
+        checked: false,
+        item: "Ice-Cream"
+    }
+];
+
+const [items,setItems] = useState(groceryList);
+
+const handleCheck = (id) => {
+  const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item)
+  setItems(listItems);
+  localStorage.setItem("shoppingList",JSON.stringify(listItems))
+};
+
+const handleDelete = (id) => {
+  const listItems = items.filter((item) => item.id !== id);
+  setItems(listItems);
+  localStorage.setItem("shoppingList",JSON.stringify(listItems))
+};
+
   return (
     <div>
       <Header/>
-      <Content/>
-      <Footer/>
+      <Content items={items} setItems={setItems} handleCheck={handleCheck} handleDelete={handleDelete}/>
+      <Footer length={items.length}/>
     </div>
   );
 };
